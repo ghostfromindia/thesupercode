bee3d8d75cbb435080f494c47c7bbff812af72ee
Akhil Joy <b2akhilmj@gmail.com>
2020-12-21 19:17:19 +0530

    Channel page update

--- resources/views/client/modules/youtube/channel.blade.php
+++ resources/views/client/modules/youtube/channel.blade.php
@@ -11,14 +11,14 @@
 
 
     <!--hero section start-->
-    <section class="hero-equal-height ptb-120 gradient-overlay bg-image">
+    <section class="hero-equal-height ptb-120 gradient-overlay bg-image" style="height: 50%;min-height: 0">
         <div class="background-image-wraper custom-overlay" style="background: url('{{asset('client/desktop')}}/assets/img/hero-offer-bg.svg')no-repeat center center / cover; opacity: 1;"></div>
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-9 col-lg-8">
                     <div class="hero-content-left text-white text-center">
                         <h1 class="text-white big-text mb-0"><span>Profile of {{$channel->channel_name}}</span> {{subscriberFormat($channel->subscriber_count)}}</h1>
-                        <a href="https://youtube.com/channel/{{$channel->channel_id}}" class="btn btn-danger btn-lg mb-3">View YouTube Channel</a>
+                        <a href="https://youtube.com/channel/{{$channel->channel_id}}" class="btn btn-danger btn-lg mb-3" target="_blank">View YouTube Channel</a>
                     </div>
                 </div>
             </div>
@@ -34,18 +34,21 @@
             <div class="row">
                 @foreach($stats as $obj)
                     <div class="col-12">
-                        <div>
-<div style="display: inline-block"><img src="https://img.icons8.com/officel/50/000000/planner.png" width="20px"/>  {{\Carbon\Carbon::parse($obj->statistics_date)->format('d F Y')}}</div>
-
-
-                            @if($obj->yesterday())
-                            New views : {{subscriberFormat($obj->view_count - $obj->yesterday()->view_count)}}
-                            @else
-                            Total view : {{subscriberFormat($obj->view_count)}}
-                            @endif
-
-                            Subscribes : {{subscriberFormat($obj->subscriber_count)}}
+                        <div class="features-box border p-4 rounded">
+                            <div class="features-box-icon mb-3">
+                                @if($obj->yesterday())
+                                    New views : {{subscriberFormat($obj->view_count - $obj->yesterday()->view_count)}}
+                                @else
+                                    Total view : {{subscriberFormat($obj->view_count)}}
+                                @endif
+                            </div>
+                            <div class="features-box-content">
+                                <h5> {{\Carbon\Carbon::parse($obj->statistics_date)->format('d F Y')}}</h5>
+                                {{--<p>Our intuitive control panel gives you admin access to all of your DreamHost products--}}
+                                    {{--easily.</p>--}}
+                            </div>
                         </div>
+
                     </div>
                 @endforeach
             </div>
