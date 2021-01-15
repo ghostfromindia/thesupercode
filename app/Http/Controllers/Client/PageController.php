250cd90d62b1fdcffc4ded21a59d333128c97f6e
Akhil Joy <b2akhilmj@gmail.com>
2021-01-15 11:42:09 +0530

    favicon and icon update

--- app/Http/Controllers/Client/PageController.php
+++ app/Http/Controllers/Client/PageController.php
@@ -18,4 +18,14 @@ class PageController extends Controller
     public function image_upload(){
         $this->uploadit_link('https://yt3.ggpht.com/ytc/AAUvwnjipJKbPXIbI0KeW_o49gCuiHLaJZVdHrPn1WWeBw=s800-c-k-c0x00ffffff-no-rj','files/youtube/channels','youtuber-image-1','youtube');
     }
+
+    public function terms_condition(){
+        return view('client.pages.terms_condition');
+    }
+
+    public function privacy_policy(){
+        return view('client.pages.privacy_policy');
+    }
+
+
 }
